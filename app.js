1a9d944d1ec006be41fc96fcc5ae0857b8996b83
Juan Foncuberta Calbeto <juan@juanfoncuberta.com>
2017-12-09 21:19:03 +0100

    Creacion de mongooseConector

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var users = require('./routes/users');
 
 var app = express();
 
+require('./lib/mongooseConnector');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
