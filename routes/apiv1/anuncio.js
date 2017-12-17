3749f372d3c7b3a57a7df12e2ad002f5d7324aaa
Juan Foncuberta Calbeto <juan@juanfoncuberta.com>
2017-12-17 01:01:06 +0100

    validacion de campos v1

--- routes/apiv1/anuncio.js
+++ routes/apiv1/anuncio.js
@@ -1,28 +1,37 @@
 "use strict";
 
-const express = require('express');
+const express = require('express'),i18n = require("i18n");
 const router = express.Router();
 const jwtAuth = require('../../lib/jwtAuth');
 const Anuncio = require('../../models/Anuncios');
+const {check,validationResult} = require('express-validator/check');
+const {matchedData,santize} = require('express-validator/filter');
 
 router.use(jwtAuth());
+router.use(i18n.init);
+
 /**
  * GET /anuncio
  * Obtener lista de anuncios
  */
-router.get('/',async (req,res,next)=>{
+router.get('/',[check('sale').optional().isBoolean().withMessage(i18n.__('must_be_boolean'))
+],async (req,res,next)=>{
+    
 
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({success:false, error: `${errors.array()[0].param} - ${errors.array()[0].msg}`});
+    }
+
+ 
     try{
         const name = req.query.name;
         const sale = req.query.sale;
-        const photo = req.query.photo;
         const tag = req.query.tag;
         const limit = parseInt(req.query.limit);
         const skip = parseInt(req.query.skip);
         const sort = req.query.sort;
-        const fields = req.query.fields;
-        const maxPrice = req.query.maxPrice;
-        const minPrice = req.query.minPrice;
+      
       //  const p = req.query.price.split('-');
        // console.log('pppppp',req.query.price.split('-').length);
 
@@ -31,28 +40,22 @@ router.get('/',async (req,res,next)=>{
         const filter ={};
         let price = {};
         if(req.query.price){
-        const p = req.query.price.split('-');
-        
-        if(p.length ==1){
-            price =p[0];
-        }else{
-           
-            price['$lte']=p[1] || Number.MAX_SAFE_INTEGER;
+            const p = req.query.price.split('-');
             
-            
-            price['$gte']=p[0] || 0;
+            if(p.length ==1){
+                price =p[0];
+            }else{       
+                price['$lte']=p[1] || Number.MAX_SAFE_INTEGER;
+                price['$gte']=p[0] || 0;
 
+            }
         }
-    }
         if(name){   filter.name = new RegExp('^' + name, "i");; }
-        if(photo){  filter.photo = photo;   }
         if(tag){    filter.tags = {$in:tag.split(' ')}}
         if(sale){   filter.sale =sale   }
-        //if(maxPrice){ price['$lte']=maxPrice}
-        //if(minPrice){ price['$gte']=minPrice}
         if(Object.keys(price).length){filter.price = price;};
        
-       console.log(filter);
+    
         
         const listado = await Anuncio.list(filter);
         for(let i = 0;i<listado.length;i++){
--- routes/apiv1/usuario.js
+++ routes/apiv1/usuario.js
@@ -3,29 +3,63 @@ const crypto = require('crypto');
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
+const i18n = require("i18n");
+
+const {check,validationResult} = require('express-validator/check');
+const {matchedData,santize} = require('express-validator/filter');
+
 
 const Usuario = require('../../models/Usuarios');
 
-router.post('/',async(req,res,next)=>{
+router.post('/',
+[check('email').exists().withMessage(i18n.__('is_required')).isEmail().withMessage(i18n.__('must_be_email')),
+check('name').optional().isLength({ min: 1 }).withMessage(i18n.__('not_empty')),
+check('password').exists().withMessage(i18n.__('is_required'))
+],async(req,res,next)=>{
     //creamos un usuario en memoria
+    //req.checkBody("email", "Enter a valid email address.").isEmail();
+    const errors = validationResult(req);
+   if (!errors.isEmpty()) {
+      return res.status(422).json({success:false, error: `${errors.array()[0].param} - ${errors.array()[0].msg}`});
+    }
+ 
 let token = '';  
     try{
-        
+    
+      if(req.get('Accept-Language'))
+            i18n.setLocale(req.get('Accept-Language'));
+   
         req.body.password = crypto.createHash('md5').update(req.body.password).digest('hex');
         if(req.body.name){
+            const checkUser =await Usuario.listOne({email:req.body.email});
+            if(checkUser){
+                res.json({success:false,error:i18n.__('email_exist')});
+                return;
+
+            }
             const usuario = new Usuario(req.body);
-            usuario.save((err,nuevoUsuario)=>{
+
+            usuario.save(async (err,nuevoUsuario)=>{
+             
                 if(err){
                     next(err);
                     return;
                 }
-                token = makeToken(nuevoUsuario,next);             
+                token = await makeToken(nuevoUsuario,next);
+                res.json({success:true,token:token});             
             });
         }else{            
             const logUser = await Usuario.listOne(req.body);
+            
+            if(!logUser){
+                res.json({success:false,error:i18n.__('user_not_found')});
+                return;
+            }
             token = await makeToken(logUser,next);
+            res.json({success:true,token:token});
         }
-        res.json({success:true,token:token});       
+     
+               
         return;
        
     }catch(err){
@@ -34,11 +68,10 @@ let token = '';
 
 });
 
-
 router.get('/',async (req,res,next)=>{
     
         try{
-            
+        
             const listado = await Usuario.list();
        
             res.json({success:true,result:listado});
@@ -51,12 +84,10 @@ router.get('/',async (req,res,next)=>{
     });
 
 
-
-//process.env.JWT_SECRET
 function makeToken(usuario,next){
 
     return new Promise((resolve,reject)=>{
-        console.log('gol',process.env.JWT_EXPIRES_IN);
+      
         const tokenUser={_id:usuario.id,mail:usuario.mail}
         jwt.sign({user_id: tokenUser._id},process.env.JWT_SECRET,{
             expiresIn:process.env.JWT_EXPIRES_IN,
