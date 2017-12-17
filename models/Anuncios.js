7fe1dfbc55e2a82df7b04c29a7703c1d53aad884
Juan Foncuberta Calbeto <juan@juanfoncuberta.com>
2017-12-17 01:04:23 +0100

    modelo anuncio

--- models/Anuncios.js
+++ models/Anuncios.js
@@ -1,13 +1,28 @@
 "use strict";
 
 const mongoose = require('mongoose');
-
+const maxTags = 4;
 const anuncioSchema = mongoose.Schema({
-    name:String,
-    sale:Boolean,
-    price:Number,
-    photo:String,
-    tags:[String]
+    name:{
+        type:String,
+        required:true
+    },
+    sale:{
+        type:Boolean,
+        required:true,
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    photo:{
+        type:String,
+        required:true
+    },
+    tags:{
+        type:[String],
+        enum: ['work,', 'lifestyle,', 'motor','mobile']
+    }
 
 });
 
@@ -17,6 +32,11 @@ anuncioSchema.statics.list = function(filter){
     return query.exec();
 }
 const Anuncio = mongoose.model('Anuncio',anuncioSchema);
+
+
+function arrayLimit(val){
+    return val.length <=4;
+}
 
 module.exports = Anuncio;
 
