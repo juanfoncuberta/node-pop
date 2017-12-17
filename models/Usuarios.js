0ebc25c6960c008cf3bd8800f799dfcad55a9845
Juan Foncuberta Calbeto <juan@juanfoncuberta.com>
2017-12-17 01:02:10 +0100

    modelo usuario

---
+++ models/Usuarios.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const usuarioSchema = mongoose.Schema({
+    name:{
+        type: String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+
+});
+
+usuarioSchema.statics.list = function(filter){
+    const query = this.find(filter);
+
+    return query.exec();
+}
+
+usuarioSchema.statics.listOne = function(filter){
+    
+    const query = this.findOne(filter);
+
+    return query.exec();;
+}
+const Usuario = mongoose.model('Usuario',usuarioSchema);
+
+module.exports = Usuario;
+
+
